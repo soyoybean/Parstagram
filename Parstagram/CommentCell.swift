ab68c78799ab7a86d3b07281c4f200c0386eea66
soyoybean <sok020@ucsd.edu>
2020-02-28 20:40:02 -0800

    Display Comments

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,27 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Soyon Kim on 2/28/20.
+//  Copyright © 2020 Soyon Kim. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
