807168088d845e20af5d1fd1cac134799ac4fa41
soyoybean <sok020@ucsd.edu>
2020-02-28 19:11:28 -0800

    Log out

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -74,5 +74,16 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        let sceneDelegate = self.view.window?.windowScene?.delegate as! SceneDelegate
+        
+        sceneDelegate.window?.rootViewController = loginViewController
+    }
 
 }
